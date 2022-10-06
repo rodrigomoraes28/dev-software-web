c602716eac8f18d44103344e81bd0a825cc811f8
MichelSouzaGit <michels.git@gmail.com>
2022-10-06 17:52:01 -0300

    Componente NavBar (separado do App.js)

--- student_Teacher/src/App.js
+++ student_Teacher/src/App.js
@@ -3,51 +3,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import { Rotas } from './components/routes/routes.jsx'
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
+import NavBar from './components/navbar/NavBar'
 
 
 function App() {
   return (
     <div className="container">
-      <nav className='navbar navbar-expand-lg navbar-white bg-info'>
-        <Link to='/' className='navbar-brand' style={{ paddingLeft: 10 }}> CRUD </Link>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav mr-auto'>
-            <li className='nav-item'>
-              <Link to='/' className='nav-link'> Home </Link>
-            </li>
-
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="/#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Estudante
-              </a>
-              <ul className="dropdown-menu">
-                <li className='nav-item'>
-                  <Link to='/createStudent' className='nav-link'> Criar Estudante </Link>
-                </li>
-                <li className='nav-item'>
-                  <Link to='/listStudent' className='nav-link'> Listar Estudante </Link>
-                </li>
-              </ul>
-            </li>
-
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="/#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Professor
-              </a>
-              <ul className="dropdown-menu">
-                <li className='nav-item'>
-                  <Link to='/createTeacher' className='nav-link'> Criar Professor </Link>
-                </li>
-                <li className='nav-item'>
-                  <Link to='/listTeacher' className='nav-link'> Listar Professor </Link>
-                </li>
-              </ul>
-            </li>
-
-          </ul>
-        </div>
-      </nav>
+      < NavBar />
       < Rotas />
     </div>
   );
