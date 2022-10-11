ffe19d48d75a0adf37e07f6efe2c893c72d9b440
MichelSouzaGit <michels.git@gmail.com>
2022-10-11 19:44:17 -0300

    Questão 2 finalizada e adicionada

---
+++ avaliacaoParcial/questao2/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import Calculadora from './components/Questao02/Calculadora'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Calculadora />
+    </div>
+  );
+}
+
+export default App;
