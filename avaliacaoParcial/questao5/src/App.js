090867f7780946416aa25f9bbe4f95799f7e614a
MichelSouzaGit <michels.git@gmail.com>
2022-10-20 00:05:10 -0300

    Questão 05 adicionada e funcinando

---
+++ avaliacaoParcial/questao5/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Questao05 from './components/Questao05/Questao05';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Questao05 />
+    </div>
+  );
+}
+
+export default App;
